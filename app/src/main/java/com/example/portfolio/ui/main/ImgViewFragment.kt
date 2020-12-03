36f02a8d4b9ccb0fb81170c86fe56cf1c9dd0b67
lafared <lafared80@gmail.com>
2020-12-04 02:45:48 +0900

    initial commit

---
+++ app/src/main/java/com/example/portfolio/ui/main/ImgViewFragment.kt
@@ -0,0 +1,29 @@
+package com.example.portfolio.ui.main
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.portfolio.R
+
+class ImgViewFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = ImgViewFragment()
+    }
+
+    private lateinit var viewModel: MainViewModel
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View {
+        return inflater.inflate(R.layout.imgview_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+    }
+
+}
